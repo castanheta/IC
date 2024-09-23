04c413dc2011c4ed7257c716152d80de0f878539
Afonso Castanheta <63506060+castanheta@users.noreply.github.com>
2024-09-23 14:16:55 +0100

    Create T1.cpp

---
+++ T1.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // Include the standard input-output stream library
+
+int main() {
+    // Output "Hello, World!" to the console
+    std::cout << "Hello, World!" << std::endl;
+    
+    return 0; // Indicate that the program ended successfully
+}
